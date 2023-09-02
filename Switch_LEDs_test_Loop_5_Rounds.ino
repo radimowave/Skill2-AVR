6983ff2c00ba22de445c01ea1e3dc950f8a27091
MetaRadiator <35647950+radimowave@users.noreply.github.com>
2023-09-02 14:35:23 +0700

    Create Switch_LEDs_test_Loop_5_Rounds.ino

---
+++ Switch_LEDs_test_Loop_5_Rounds.ino
@@ -0,0 +1,107 @@
+int button1 = 2;
+int button2 = 4;
+int button3 = 7;
+int button4 = 8;
+int LED1 = 3;
+int LED2 = 5;
+int LED3 = 6;
+int LED4 = 9;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(button1, INPUT_PULLUP);
+  pinMode(button2, INPUT_PULLUP);
+  pinMode(button3, INPUT_PULLUP);
+  pinMode(button4, INPUT_PULLUP);
+
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(LED4, OUTPUT);
+
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED4, LOW);
+
+}
+
+void loop() {
+  if (digitalRead(button1) == LOW) {
+    Serial.print("Logic = ");Serial.print(digitalRead(button1));Serial.print("  ");
+        for (int i=0; i<=4; i++) {
+      Serial.printf("\n Round %d ", i+1); //ใช้ Serial.printf, \n ขึ้นบรรทัดใหม่ %d เลขจำนวนนับ
+      digitalWrite(LED1, HIGH);
+      delay(100);
+      digitalWrite(LED1, LOW);
+      delay(100);
+      digitalWrite(LED2, HIGH);
+      delay(100);
+      digitalWrite(LED2, LOW);
+      delay(100);
+      digitalWrite(LED3, HIGH);
+      delay(100);
+      digitalWrite(LED3, LOW);
+      delay(100);
+      digitalWrite(LED4, HIGH);
+      delay(100);
+      digitalWrite(LED4, LOW);
+      delay(100);
+    }
+      
+  } else if(digitalRead(button2) == LOW) {
+     Serial.print("Logic = ");Serial.print(digitalRead(button2));Serial.print("  ");
+      digitalWrite(LED4, HIGH);
+      delay(500);
+      digitalWrite(LED4, LOW);
+      delay(500);
+      digitalWrite(LED3, HIGH);
+      delay(500);
+      digitalWrite(LED3, LOW);
+      delay(500);
+      digitalWrite(LED2, HIGH);
+      delay(500);
+      digitalWrite(LED2, LOW);
+      delay(500);
+      digitalWrite(LED1, HIGH);
+      delay(500);
+      digitalWrite(LED1, LOW);
+      delay(500);
+      
+  } else if(digitalRead(button3) == LOW) {
+     Serial.print("Logic = ");Serial.print(digitalRead(button3));Serial.print("  ");
+     for (int i=0; i<=4; i++) {
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, HIGH);
+      delay(500);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+      delay(500);
+      digitalWrite(LED3, HIGH);
+      digitalWrite(LED4, HIGH);
+      delay(500);
+      digitalWrite(LED3, LOW);
+      digitalWrite(LED4, LOW);
+      delay(500);
+      }
+      
+  } else if(digitalRead(button4) == LOW) {
+     Serial.print("Logic = ");Serial.print(digitalRead(button4));Serial.print("  ");
+     for (int i=0; i<=4; i++) {
+      digitalWrite(LED2, HIGH);
+      digitalWrite(LED4, HIGH);
+      delay(500);
+      digitalWrite(LED2, LOW);
+      digitalWrite(LED4, LOW);
+      delay(500);
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED3, HIGH);
+      delay(500);
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED3, LOW);
+      delay(500);
+     }
+  }
+   
+}
